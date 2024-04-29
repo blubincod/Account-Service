224e52b50dd11f3db16199d9077d38527cd7518f
JYP <rocket.jyp@gmail.com>
2024-04-29 21:51:53 +0900

    Account_Service_v0.1.1_AOP

--- src/main/java/com/example/account/type/ErrorCode.java
+++ src/main/java/com/example/account/type/ErrorCode.java
@@ -6,12 +6,14 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    INTERNAL_SERVER_ERROR("내부 서버 오류가 발생했습니다."),
     INVALID_REQUEST("잘못된 요청입니다."),
     USER_NOT_FOUND("사용자가 없습니다."),
     ACCOUNT_NOT_FOUND("계좌가 없습니다."),
     AMOUNT_EXCEED_BALANCE("해당 거래가 없습니다."),
     TRANSACTION_ACCOUNT_UN_MATCH("이 거래는 해당 계좌에서 발생한 거래가 아닙니다."),
     TRANSACTION_NOT_FOUND("거래 금액이 계좌 잔액보다 큽니다."),
+    ACCOUNT_TRANSACTION_LOCK("해당 계좌는 사용 중입니다."),
     CANCEL_MUST_FULLY("부분 취소는 허용되지 않습니다."),
     TOO_OLD_ORDER_TO_CANCEL("1년이 지난 거래는 취소가 불가능합니다."),
     USER_ACCOUNT_UN_MATCH("사용자와 계좌의 소유자가 다릅니다."),
